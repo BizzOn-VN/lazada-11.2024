3e928218a700e7d779dd0ca31e8647d22a09cfef
chikien111 <38467294+chikien111@users.noreply.github.com>
2024-11-01 15:03:44 +0700

    update page

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/* HELPER: Checks Whether an Element Exists
+----------------------------------------------------------------------------------------------------*/
+(function( $ ) {
+
+  $.fn.extend({
+    exists: function() {
+      if ( this.length > 0 ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  });
+
+})( jQuery );
+
+
+
+jQuery(document).on("ready",function () {
+	
+});
+
+
+
+
